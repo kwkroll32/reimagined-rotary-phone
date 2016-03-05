79872e14d93ab59a8b51ed8b56653b14ec2db697
Karl Kroll <kwkroll.32@gmail.com>
2016-03-05 15:44:46 -0500

    refactor code. implement test setUp

--- SitterTests.py
+++ SitterTests.py
@@ -4,24 +4,22 @@ from datetime import time
 from Sitter import Sitter
 
 class SitterTest(unittest.TestCase):
+    def setUp(self):
+        self.startTime = time(17,0,0) # 24-hr time in hr, min, sec
+        self.endTime = time(21,0,0) # 24-hr time in hr, min, sec
+        self.bedTime = time(19,0,0) # 24-hr time in hr, min, sec
+        self.sitter = Sitter(startTime=self.startTime, endTime=self.endTime, bedTime=self.bedTime)
+
     def testAlwaysTrue(self):
         self.assertTrue(True)
         
     def testSitterInit(self):
-        startTime = time(17,0,0) # 24-hr time in hr, min, sec
-        endTime = time(21,0,0) # 24-hr time in hr, min, sec
-        bedTime = time(19,0,0) # 24-hr time in hr, min, sec
-        sitter = Sitter(startTime = startTime, endTime=endTime, bedTime=bedTime)
-        self.assertEqual(sitter.startTime, startTime)
-        self.assertEqual(sitter.endTime, endTime)
-        self.assertEqual(sitter.bedTime, bedTime)
+        self.assertEqual(self.sitter.startTime, self.startTime)
+        self.assertEqual(self.sitter.endTime, self.endTime)
+        self.assertEqual(self.sitter.bedTime, self.bedTime)
         
     def testSitterTimesAreValid(self):
-        startTime = time(17,0,0) # 24-hr time in hr, min, sec
-        endTime = time(21,0,0) # 24-hr time in hr, min, sec
-        bedTime = time(19,0,0) # 24-hr time in hr, min, sec
-        sitter = Sitter(startTime = startTime, endTime=endTime, bedTime=bedTime)
-        self.assertTrue(sitter.validTimes())
+        self.assertTrue(self.sitter.validTimes())
         
 if __name__ == "__main__":
     unittest.main()
